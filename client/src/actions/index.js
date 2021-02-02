494084a39ca17ae475fbae40a9febb7b56453648
Viktoryia <kulinkovichva@gmail.com>
2021-02-02 22:21:30 +0300

    added UpdateTask reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,32 +1,34 @@
 export const addPlan = (planObj) => {
-    return {
-        type: 'ADD_PLAN',
-        value: planObj
-    }
-}
+  return {
+    type: 'ADD_PLAN',
+    value: planObj,
+  };
+};
 
 export const deletePlan = (planId) => {
-    return {
-        type: 'DELETE_PLAN',
-        value: planId
-    }
-}
+  return {
+    type: 'DELETE_PLAN',
+    value: planId,
+  };
+};
 
 export const addTask = (taskObj) => {
-    return {
-        type: 'ADD_TASK',
-        value: taskObj
-    }
-}
+  return {
+    type: 'ADD_TASK',
+    value: taskObj,
+  };
+};
 
 export const deleteTask = (taskObj) => {
-    return {
-        type: 'DELETE_TASK',
-        value: taskObj
-    }
-}
+  return {
+    type: 'DELETE_TASK',
+    value: taskObj,
+  };
+};
 
-
-
-
-
+export const updateTask = (taskObj) => {
+  return {
+    type: 'UPDATE_TASK',
+    value: taskObj,
+  };
+};
