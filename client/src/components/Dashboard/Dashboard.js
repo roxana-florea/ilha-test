019234d246220627491397bd6a5588bda57e464c
xuleasca <florea.roxana5@yahoo.com>
2021-02-04 18:48:57 +0000

    fixed drawer for small screen

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -31,8 +31,8 @@ import UserAvatar from 'react-user-avatar';
 import Avatar from '@material-ui/core/Avatar';
 
 
-import UserProfile from '../userProfile/UserProfile';
-import photo from '../images/userIconMale.jpg';
+// import UserProfile from '../userProfile/UserProfile';
+// import photo from '../images/userIconMale.jpg';
 import './Dashboard.css';
 
 const drawerWidth = 200;
@@ -129,6 +129,28 @@ export default function MiniDrawer() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
 
+  ///////////////////// make drawer close when screen is small
+const setSmall = () => {
+    setOpen(false);
+}
+
+const setLarge = () => {
+    setOpen(true);
+}
+
+useEffect(()=>{
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  
+  mediaQuery.addListener((mq) => {
+    if (mq.matches) {
+      setLarge()
+    } else {
+      setSmall()
+    }
+  });
+})
+
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -176,12 +198,14 @@ export default function MiniDrawer() {
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
           [classes.drawerClose]: !open,
+             
         })}
         classes={{
           paper: clsx({
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
           }),
+
         }}
 
       >
