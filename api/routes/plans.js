6cc14c243044f18587b20723ba27ff84907f65db
AyaDesigner <aya.berdyeva@gmail.com>
2021-02-15 20:59:09 +0100

    update and delete tasks routes added

--- api/routes/plans.js
+++ api/routes/plans.js
@@ -2,37 +2,37 @@ const router = require('express').Router();
 const Plan = require('../models/plans');
 
 router.route('/').get((req, res) => {
-  Plan.find()
-    .then(plans => res.json(plans))
-    .catch(err => res.status(400).json('Error: ' + err));
+    Plan.find()
+        .then(plans => res.json(plans))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/').post((req, res) => {
-  const planName = req.body.planName;
-  const tasks = req.body.tasks;
-//   plan.date = Date.parse(req.body.date);
-  const newPlan = new Plan({planName, tasks});
+    const planName = req.body.planName;
+    const tasks = req.body.tasks;
+    //   plan.date = Date.parse(req.body.date);
+    const newPlan = new Plan({ planName, tasks });
 
-  newPlan.save()
-  .then(() => res.json('Plan added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
-  
+    newPlan.save()
+        .then(() => res.json('Plan added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
 });
 
 
 
 
 router.route('/:id').get((req, res) => {
-  Plan.findById(req.params.id)
-    .then(plan => res.json(plan))
-    .catch(err => res.status(400).json('Error: ' + err));
+    Plan.findById(req.params.id)
+        .then(plan => res.json(plan))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 
 router.route('/:id').delete((req, res) => {
-  Plan.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Plan deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    Plan.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Plan deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 /**
@@ -42,18 +42,34 @@ router.route('/:id').delete((req, res) => {
  * Finally, we call plan.save to save the updated object in the database.
  */
 router.route('/:id').put((req, res) => {
-  Plan.findById(req.params.id)
-    .then(plan => {
-      plan.plaName = req.body.planName;
-      plan.save()
-        .then(() => res.json('plan updated!'))
+    Plan.findById(req.params.id)
+        .then(plan => {
+            plan.plaName = req.body.planName;
+            plan.save()
+                .then(() => res.json('plan updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
         .catch(err => res.status(400).json('Error: ' + err));
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:plan_id/tasks/:task_id').put((req, res) => {
+    Plan.findById(req.params.plan_id).update(
+
+        { "tasks._id": req.params.task_id },
+        { "$set": { "tasks.$": req.body } }
+        
+    ).then(() => res.json('Task updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 
-     
+router.route('/:plan_id/tasks/:task_id').delete((req, res) => {
+    Plan.findByIdAndDelete(req.params.plan_id).update(
 
+        { "$pull": { tasks: { "_id": req.params.task_id } } }
+
+    ).then(() => res.json('Task deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
