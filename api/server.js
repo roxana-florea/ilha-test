3cdf0780a3e7c75e8d4fe1d90e6c27c562c80c31
Viktoryia <kulinkovichva@gmail.com>
2021-02-15 18:37:55 +0300

    added peerjs connection to videoroom

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const app = require('./app');
+const { ExpressPeerServer } = require('peer');
 const port = process.env.PORT || 5000;
 
-app.listen(port, (err) => {
-    if(err){
-        console.log("Error occured");
-    }
-    else{
-        console.log(`Successfuly connected to to port: ${port}`);
-    }
-})
+const server = app.listen(port, (err) => {
+  if (err) {
+    console.log('Error occured');
+  } else {
+    console.log(`Successfuly connected to to port: ${port}`);
+  }
+});
+
+const peerServer = ExpressPeerServer(server);
+app.use('/peerjs', peerServer);
